36577e38dd2091939486f78efc358e5fb08752d9
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-01 20:05:55 -0600

    se agrega la direccion hacia la pagina de contacto

--- Model/EnlacesModel.php
+++ Model/EnlacesModel.php
@@ -8,6 +8,7 @@ class EnlacesModel{
         $modulo =match($enlace){
             "inicio"=>"View/pages/inicio.php",
             "nosotros"=>"View/pages/nosotros.php",
+            "contacto"=>"View/pages/contacto.php",
             default => "View/pages/error.php"
 
         };
