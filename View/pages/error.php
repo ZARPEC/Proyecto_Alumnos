0c5090dcec287b9b6e964b9ac3690295ea263082
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-01 18:51:46 -0600

    pagina de error para paginas inexistentes

---
+++ View/pages/error.php
@@ -0,0 +1,10 @@
+<div class="d-flex align-items-center justify-content-center">
+    <div class="text-center">
+        <h1 class="display-1 fw-bold">404</h1>
+        <p class="fs-3"> <span class="text-danger">Opps!</span> pagina no encontrada.</p>
+        <p class="lead">
+            La pagina que buscas no existe.
+        </p>
+        <a href="index.php" class="btn btn-primary">Ir al inicio</a>
+    </div>
+</div>
