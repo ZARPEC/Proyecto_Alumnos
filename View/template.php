d51643f96826706a5334facd0406a4608e5803b5
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-01 18:41:20 -0600

    css diseño de pagina

--- View/template.php
+++ View/template.php
@@ -13,7 +13,7 @@ $capturaEnlace = new PaginaController;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="View/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="/resources/demos/style.css">
