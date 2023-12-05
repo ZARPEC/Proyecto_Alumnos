0869288a29a7d82103820da41f960995b57a3761
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-04 21:25:40 -0600

    se agrego un elemento select para mostrar los roles

--- View/users/nuevoUsuario.php
+++ View/users/nuevoUsuario.php
@@ -1,7 +1,8 @@
 <?php
 
 use Controller\UsuarioController;
-$usuario= new UsuarioController;
+$usuario = new UsuarioController;
+$rol = $usuario;
 
 ?>
 
@@ -10,7 +11,7 @@ $usuario= new UsuarioController;
 
     <form method="POST">
 
-    <div class="form-group">
+        <div class="form-group">
             <div class="row mb-3">
                 <div class="col-2"><label>Nombre</label></div>
                 <div class="col-10"><input class="form-control" type="text" name="nombre" required></div>
@@ -38,32 +39,47 @@ $usuario= new UsuarioController;
             </div>
         </div>
 
+
+        <div class="form-group">
+            <div class="row mb-3">
+                <div class="col-2"><label>Categoria</label></div>
+                <div class="col-5">
+                    <select name="rol" class="form-select">
+                        <?php
+                        foreach ($rol->mostrarRol() as $row => $item) {
+                            echo "<option value='{$item['id']}'>{$item['rol']}</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+        </div>
+
         <div class="form-group">
             <div class="row">
                 <button type="submit" class="btn btn-outline-success btn-sm">Crear Cuenta</button>
             </div>
         </div>
 
-        
 
-    <input type="hidden" name="rol" value="c">
+
+
 
     </form>
     <div class="mt-4">
 
     </div>
     <?php
-    $resultado=$usuario->UsuarioNuevo();
-    if($resultado=="Error"){
+    $resultado = $usuario->UsuarioNuevo();
+    if ($resultado == "Error") {
 
         echo "<div class='alert alert-danger mt-5' role='alert'>
         Error en los datos
         </div>";
-    }else if($resultado=="guardado"){
+    } else if ($resultado == "guardado") {
         echo "<div class='alert alert-dismissible alert-success mt-5' role='alert'>
         Usuario creado correctamente
         </div>";
-        
     }
 
     ?>
