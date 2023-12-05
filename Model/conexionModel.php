c4967ed7acdb9c824df57e35de169edd2b77fec6
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-04 21:23:48 -0600

    se modifico la direccion hacia la base de datos

--- Model/conexionModel.php
+++ Model/conexionModel.php
@@ -6,7 +6,7 @@ class ConexionModel
 {
     public static function conectar()
     {
-        $conn = new \PDO("mysql:host=127.0.0.1:3307;dbname=biblioteca", "root", "");
+        $conn = new \PDO("mysql:host=127.0.0.1:3307;dbname=alumnos", "root", "");
 
         return $conn;
     }
