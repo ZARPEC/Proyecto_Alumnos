d09b34a9dd4edfc637e4fe32a750ae5ece05b9af
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-05 20:18:12 -0600

    se crea el modelo y el controlador para los profesores

---
+++ Controller/profesorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controller;
+
+use Model\profesorModel;
+
+class profesorController
+{
+
+    public function agregarProfesor()
+    {
+
+        if (!empty($_POST['nombre']) && !empty($_POST['apellido']) && !empty($_POST['fkusuario'])) {
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $fkusuario = $_POST['fkusuario'];
+
+            $datos = array(
+                'nombre'=>$nombre,
+                'apellido' => $apellido,
+                'fkusuario' => $fkusuario
+            );
+            $respuesta = profesorModel::nuevoProfesor($datos);
+
+            return $respuesta ? "guardado" : "error";
+        }
+    }
+}
---
+++ Model/profesorModel.php
@@ -0,0 +1,22 @@
+<?php
+namespace Model;
+
+use Model\ConexionModel;
+
+class profesorModel{
+
+    public static function nuevoProfesor($datos){
+        try {
+            $stmt = ConexionModel::conectar()->prepare("INSERT INTO profesor(nombre,apellido,fkusuario) values(:nom,:ape,:fkuser)");
+            $stmt->bindParam(":nom", $datos['nombre'], \PDO::PARAM_STR);
+            $stmt->bindParam(":ape", $datos['apellido'], \PDO::PARAM_STR);
+            $stmt->bindParam(":fkuser", $datos['fkusuario'], \PDO::PARAM_STR);
+            return $stmt->execute() ? true : false;
+        } catch (\Throwable $th) {
+            echo $th;
+            return false;
+        }
+    }
+}
+
+?>
