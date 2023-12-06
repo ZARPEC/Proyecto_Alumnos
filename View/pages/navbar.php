d0e7d5214966d147c884c53a851187ac110909dd
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-05 20:19:10 -0600

    se agrega lista para la modificacion de profesores, unicamente administradores pueden ver

--- View/pages/navbar.php
+++ View/pages/navbar.php
@@ -12,7 +12,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav" >
+            <div class="navbar-nav">
                 <a class="nav-link active  text-light" aria-current="page" href="index.php">Inicio</a>
                 <a class="nav-link  text-light" href="index.php?action=nosotros">Nosotros <i class="fa-solid fa-people-arrows fa-2xs"></i> </a>
                 <a class="nav-link  text-light" href="index.php?action=contacto">contacto</a>
@@ -20,19 +20,42 @@
                 <?php
                 if (!empty($_SESSION['id'])) { //Tiene sesión activa
                 ?>
-                    <a class="nav-link  text-light" href="index.php?action=alquilar">Alquilar<i class="bi bi-book-fill"></i> </a>
-                    <a class="nav-link  text-light" href="index.php?action=verAlquiler">libros alquilados</a>
-                    <a class="nav-link text-light " href="index.php?action=buscar">ubicacion de libro</a>
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link text-light dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Alumnos</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Modificar Alumnos</a>
+                                <a class="dropdown-item" href="#">Listado Alumnos</a>
+                                <a class="dropdown-item" href="#">Agregar alumno</a>
+                                <div class="dropdown-divider"></div>
+                                <?php
+                                if (!empty($_SESSION['rol']) && $_SESSION['rol'] == '1') {
+                                ?>
+                                    <a class="dropdown-item" href="index.php?action=agregarLibro">Listar todos los alumnos</a>
+                                <?php } ?>
+                            </div>
+                        </li>
+                    </ul>
                     <?php
-                    if(!empty($_SESSION['rol'])&& $_SESSION['rol']=='s'){
-                        ?>
-                        <a class="nav-link text-light"  href="index.php?action=agregarLibro">Agregar libro</a>
-                        <?php } ?>
+                    if (!empty($_SESSION['rol']) && $_SESSION['rol'] == '1') {
+                    ?>
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link text-light dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profesores</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Modificar Profesor</a>
+                                    <a class="dropdown-item" href="#">Listado profesores</a>
+                                    <a class="dropdown-item" href="index.php?action=agregarProfesor">Agregar profesor</a>
+                                    <div class="dropdown-divider"></div>
+                                </div>
+                            </li>
+                        </ul>
+                    <?php } ?>
                     <a class="nav-link  text-light" href="index.php?action=logout">Cerrar sesión</a>
                 <?php } else { //No ha iniciado sesión
                 ?>
                     <a class="nav-link  text-light" href="index.php?action=login">Iniciar sesión</a>
-                    <a class="nav-link  text-light" href="index.php?action=crearCuenta">Crear Cuenta</a> 
+                    <a class="nav-link  text-light" href="index.php?action=crearCuenta">Crear Cuenta</a>
                 <?php } ?>
 
 
