f7b657496c2be397dbfe0828c096aa2f5d2bb8cc
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2023-12-10 10:01:28 -0600

    sse agrega controlador para grado

---
+++ Controller/gradoController.php
@@ -0,0 +1,33 @@
+<?php
+namespace Controller;
+
+use model\gradoModel;
+
+class gradoController{
+
+    public function mostrarGrado(){
+
+        $grado=gradoModel::mostrarGrado();
+        return $grado;
+    }
+
+    public function agregarGrado() {
+        if (!empty($_POST['grado']) && !empty($_POST['fkprofesor'])) {
+            $grado = $_POST['grado'];
+            $fkprofesor = $_POST['fkprofesor'];
+
+
+            $datos = array(
+                'grado'=>$grado,
+                'fkprofesor' => $fkprofesor
+            );
+            $respuesta = gradoModel::nuevoGrado($datos);
+
+            return $respuesta ? "guardado" : "error";
+        }
+    }
+        
+    
+}
+
+?>
